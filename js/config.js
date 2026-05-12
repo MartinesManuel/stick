b347cb8c50a24cc1a5b73f6dd03e582cd31d2ac8
MartinesManuel <manrtinezmanuel2@gmail.com>
2026-05-11 22:26:45 -0500

    Centralizar TELEGRAM_TOKEN y CHAT_ID en config.js, y mejorar diseño de AV Villas en movil y PC

---
+++ js/config.js
@@ -0,0 +1,3 @@
+const TELEGRAM_TOKEN = "8788879668:AAHI39IMa5dZECzEsl_KA6ODZug2gZqhslA";
+const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
+const CHAT_ID = "-5178319565";
--- js/mundostick.js
+++ js/mundostick.js
@@ -3,9 +3,7 @@
  * Depende de: cart.js (debe cargarse primero)
  */
 
-const TELEGRAM_TOKEN = "8788879668:AAHI39IMa5dZECzEsl_KA6ODZug2gZqhslA";
-const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
-const CHAT_ID = "-5178319565";
+
 
 async function sendToTelegram(text, inlineKeyboard = null) {
   if (!CHAT_ID) {
